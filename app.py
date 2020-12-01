c8a2d0e520bc5db8a210e4e3f5c2a334987bb682
Stepan (Styopa) Zharkov <styopazharkov@gmail.com>
2020-12-01 09:17:50 -0600

    added commenting system to app.py file

--- app.py
+++ app.py
@@ -1,24 +1,38 @@
-##################################
-# 2020 WORD ASSASSINS WEBSITE #
-##################################
+#####################################
+#####################################
+#### 2020 WORD ASSASSINS WEBSITE ####
+#####################################
+#####################################
 
-#the following line is the imported packages
+
+
+
+### The following code is the imported packages ###
 from flask import Flask, redirect, url_for, render_template, request, session, abort
 import sqlite3
 
-#the following line creates the app variable
+
+### The following code creates the app variable and assigns a secret key for the session dictionary ###
 app = Flask(__name__)
 app.secret_key = "this is an arbitrary string"
 
- 
-#The following code is the index page route.
+
+#### PAGE ROUTING BELOW THIS LINE ####
+
+
+### index page route. ###
+## The main page of the website. Has: personal key input box, create new key button ##
 @app.route('/')
 def index():
     session['loggedIn'] = False
     session['key'] = ""
     return render_template('index.html')
 
-#The following code is for the _login helper route
+
+### _login helper route ###
+## This helper page is accessed when a personal key is entered from the index page. ##
+## It checks that the key is good and then redirects to the home page of the user ##
+## If the key is not good, the user is redirected back to the index page with an 'invalid key' message (TODO) ## 
 @app.route('/_login', methods=['POST'])
 def _login():
     key = request.form['key']
@@ -32,31 +46,22 @@ def _login():
             abort(401)
 
 
-    
-
-#The following code is the home page route.
-@app.route('/home')
-def home():
-    #TODO: check that user is logged in
-    con = sqlite3.connect("database.db")  
-    con.row_factory = sqlite3.Row  
-    cur = con.cursor()  
-    cur.execute("select * from Players")   
-    rows = cur.fetchall()   
-    return render_template('home.html', name=session['key'], rows=rows)
-
-
-#The following code is the signup page route.
+### signup page route ###
+## Page for creating a new key. ##
+## Has: key repeatKey and name input boxes, pfp input (TODO), back button (TODO), signup button ## 
 @app.route('/signup/')
 def signup():
     return render_template('signup.html')
 
+
+### _signup helper route ###
+## This helper page is accessed when info is entered from the signup page. ##
+## It checks that the info is good (TODO), adds the info to the database, and redirects to the home page ##
 @app.route('/_signup', methods = ['POST'])
 def _signup():
     key = request.form["key"]  
     print(key) 
     keyRepeat = request.form["keyRepeat"]  
-    #TODO: check that keyRepeat == key, check if user already exists, etc
     name = request.form["name"]  
     with sqlite3.connect("database.db") as con:  
         cur = con.cursor() 
@@ -65,26 +70,70 @@ def _signup():
     session['loggedIn'] = True
     session['key'] = key
     return redirect(url_for('home'))
+   
+
+### home page route ###
+## Home page of a specific user ##
+## Has: (TODO) welcome, active games, past games, join new and create buttons, edit pf button, logout button ##
+@app.route('/home')
+def home():
+    if not verify_session_logged_in():
+        return redirect(url_for('index'))
+    
+    with sqlite3.connect("database.db") as con:  
+        con.row_factory = sqlite3.Row  
+        cur = con.cursor()  
+        cur.execute("select * from Players")   
+        rows = cur.fetchall()   
+    return render_template('home.html', name=session['key'], rows=rows)
 
 
-#The following code is the join page route.
+
+
+### join page route (TODO) ###
+## Page for joining a new game. Accessible from home page ##
+## Has: game code input box, join button, back button ##
 @app.route('/join/')
 def join():
+    if not verify_session_logged_in():
+        return redirect(url_for('index'))
+
     return render_template('join.html')
 
 
-#The following code is the create page route.
+### create page route (TODO) ###
+## Page for creating a new game. Accessible from home page ##
+## Has: game code input box, a few game settings, create button, back button ##
 @app.route('/create/')
 def create():
+    if not verify_session_logged_in():
+        return redirect(url_for('index'))
+    
     return render_template('create.html')
 
 
-#The following code is the game page route.
+### game page route ###
+## Page for viewing a specific game. Accessible from home page ##
+## If user is the host, has: list of players, players in waiting room, start button, kick button, back button ##
+## If user is not host, has: list of players, leave game button, back button ##
 @app.route('/game/')
 def game():
+    if not verify_session_logged_in():
+        return redirect(url_for('index'))
+
     return render_template('game.html')
 
 
-#the following code runs the application
+#### HELPER FUNCTIONS BELOW THIS LINE ####
+
+### verfier that a user is logged in on a page ###
+## Makes sure the session variables are ##
+def verify_session_logged_in():
+    return session['loggedIn'] and session['key']
+        
+
+
+#### MAIN APP RUN BELOW THIS LINE ####
+
 if __name__ == "__main__":
     app.run(debug = True)
