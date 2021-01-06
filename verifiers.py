abde4d277d1ea167106969bae4fd3a9e7dbeee70
Stepan (Styopa) Zharkov <styopazharkov@gmail.com>
2021-01-06 15:44:35 -0600

    verifiers restructure finished

--- verifiers.py
+++ verifiers.py
@@ -9,32 +9,24 @@ from models import Player, PastGame, Game
 def verify_session_logged_in():
     if not (session.get('loggedIn') and session.get('user') and session.get('password')): #checks that loggedIn and user session variables exist
         return False
-    with sqlite3.connect("database.db") as con:  #checks that the user is an actual user in the database
-        con.row_factory = sqlite3.Row
-        cur = con.cursor()
-        if cur.execute("SELECT count(*) FROM Players WHERE user = ? ", (session['user'], )).fetchone()[0] == 0: #checks that user exists 
-            return False
-        if not hashing.verify(session['password'], cur.execute("SELECT * FROM Players WHERE user = ? ", (session['user'], )).fetchone()['password']) : # checks that passwords match TODO: hashpass
-            return False
+    foundPlayer = Player.query.filter_by(user = session['user']).first()
+    if not foundPlayer: #checks that user exists 
+        return false
+    if not hashing.verify(session['password'], foundPlayer.password): # checks that passwords match
+        return False
     return session['loggedIn'] #makes sure logged in variable is set to true
 
-### verifies that a user is an actual player in the game ###
+### verifies that a user is an actual player in the (active) game ###
 def verify_user_in_game(user, code):
-    with sqlite3.connect("database.db") as con:  
-        con.row_factory = sqlite3.Row
-        cur = con.cursor() 
-        if cur.execute("SELECT count(*) FROM Games WHERE code = ? ", (code, )).fetchone()[0] == 0:
-            return False
-        return user in cur.execute("SELECT * FROM Games WHERE code = ? ", (code, )).fetchone()['players']
+    foundGame = Game.query.filter_by(code = code)
+    if not foundGame:
+        return False
+    return user in json.loads(foundGame.players)
 
 ## verifies that the session user is the host ##
 def verify_host(code):
     user = session['user']
-    with sqlite3.connect("database.db") as con:  
-        con.row_factory = sqlite3.Row
-        cur = con.cursor() 
-        if cur.execute("SELECT count(*) FROM Games WHERE code = ? ", (code, )).fetchone()[0] > 0:
-            return user == cur.execute("SELECT * FROM Games WHERE code = ? ", (code, )).fetchone()['host']
-        if cur.execute("SELECT count(*) FROM PastGames WHERE code = ? ", (code, )).fetchone()[0] > 0:
-            return user == cur.execute("SELECT * FROM PastGames WHERE code = ? ", (code, )).fetchone()['host']
+    foundGame = Game.query.filter_by(code = code)
+    if not foundGame:
         return False
+    return user == foundGame.host
